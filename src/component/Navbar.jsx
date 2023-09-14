efd996e6594201b07d5fbf9be87cce963a595fda
mumuttt <peerayaforwork.f@gmail.com>
2023-09-14 11:44:49 +0700

    edittailwindhomenormal

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,12 +5,15 @@ const Navbar = () => {
   return (
     <>
     <div>
-      <ul>
-        <li>
-           <Link to={"/"}>Home</Link>
+      <ul className='flex justify-end border h-16 bg-pink-100 ' >
+        <li 
+        className='mt-4 mx-5 infotext-black-500 transition duration-200 hover:text-red-500 hover:ease-in-out focus:text-neutral-200'>
+           <Link className='text-xl ' 
+           to={"/"}>Home</Link>
         </li>
-        <li>
-           <Link to={"/HomeOwner"}>Owner</Link>
+        <li className='mt-4 mx-5 infotext-black-500 transition duration-200 hover:text-red-500 hover:ease-in-out focus:text-neutral-200'>
+           <Link className='text-xl ' 
+           to={"/HomeOwner"}>Owner</Link>
         </li>
       </ul>
     </div>
